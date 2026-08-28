5bcffe6ac114118cdcc25e1e28a63c99cd5e9120
SCaD <gestor.isp@gmail.com>
2026-08-28 13:04:08 -0600

    Agrega acceso a Catálogo de cursos

--- app.js
+++ app.js
@@ -6,13 +6,15 @@
   if (!app) return;
 
   const URLS = {
-    misCursos: 'https://www.scad.mx/members-area/gestor-isp3068/challenges'
+    misCursos: 'https://www.scad.mx/members-area/gestor-isp3068/challenges',
+    catalogoCursos: 'https://www.scad.mx/e-learning'
   };
 
   app.innerHTML = `
     <h1>CPC e-Learning</h1>
     <nav aria-label="Módulos CPC e-Learning">
       <a href="${URLS.misCursos}">Mis Cursos</a>
+      <a href="${URLS.catalogoCursos}">Catálogo de cursos</a>
     </nav>
   `;
 })();
