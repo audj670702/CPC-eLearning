0b74496f6d71257db0ed44f283c98fb985a9f4e1
SCaD <gestor.isp@gmail.com>
2026-08-28 13:03:45 -0600

    Configura acceso inicial a Mis Cursos

--- app.js
+++ app.js
@@ -5,5 +5,14 @@
 
   if (!app) return;
 
-  app.innerHTML = '<h1>CPC e-Learning</h1>';
+  const URLS = {
+    misCursos: 'https://www.scad.mx/members-area/gestor-isp3068/challenges'
+  };
+
+  app.innerHTML = `
+    <h1>CPC e-Learning</h1>
+    <nav aria-label="Módulos CPC e-Learning">
+      <a href="${URLS.misCursos}">Mis Cursos</a>
+    </nav>
+  `;
 })();
