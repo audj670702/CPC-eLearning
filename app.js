0d67ec162929f6c9f55cf011ad8a7da5f400f2d8
SCaD <gestor.isp@gmail.com>
2026-08-28 13:04:34 -0600

    Configura acceso a GYM Entrenamiento

--- app.js
+++ app.js
@@ -7,7 +7,8 @@
 
   const URLS = {
     misCursos: 'https://www.scad.mx/members-area/gestor-isp3068/challenges',
-    catalogoCursos: 'https://www.scad.mx/e-learning'
+    catalogoCursos: 'https://www.scad.mx/e-learning',
+    gymEntrenamiento: 'https://gym.scad.mx/'
   };
 
   app.innerHTML = `
@@ -15,6 +16,7 @@
     <nav aria-label="Módulos CPC e-Learning">
       <a href="${URLS.misCursos}">Mis Cursos</a>
       <a href="${URLS.catalogoCursos}">Catálogo de cursos</a>
+      <a href="${URLS.gymEntrenamiento}">GYM Entrenamiento</a>
     </nav>
   `;
 })();
