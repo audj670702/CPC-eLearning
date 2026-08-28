c83a82812af0a92bb9aacda93388e03245e14a12
SCaD <gestor.isp@gmail.com>
2026-08-28 13:08:41 -0600

    Integra monitor TV Digital Internet en Home CPC

--- app.js
+++ app.js
@@ -8,15 +8,31 @@
   const URLS = {
     misCursos: 'https://www.scad.mx/members-area/gestor-isp3068/challenges',
     catalogoCursos: 'https://www.scad.mx/e-learning',
-    gymEntrenamiento: 'https://gym.scad.mx/'
+    gymEntrenamiento: 'https://gym.scad.mx/',
+    monitorTv: 'https://qrotv.scad.mx/?monitor=1'
   };
 
   app.innerHTML = `
-    <h1>CPC e-Learning</h1>
-    <nav aria-label="Módulos CPC e-Learning">
-      <a href="${URLS.misCursos}">Mis Cursos</a>
-      <a href="${URLS.catalogoCursos}">Catálogo de cursos</a>
-      <a href="${URLS.gymEntrenamiento}">GYM Entrenamiento</a>
-    </nav>
+    <main class="home-cpc">
+      <h1>CPC e-Learning</h1>
+
+      <section class="tv-monitor" aria-label="Monitor TV Digital Internet">
+        <div class="tv-monitor-frame">
+          <iframe
+            src="${URLS.monitorTv}"
+            title="TV Digital Internet"
+            loading="eager"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </section>
+
+      <nav class="cpc-modules" aria-label="Módulos CPC e-Learning">
+        <a href="${URLS.misCursos}">Mis Cursos</a>
+        <a href="${URLS.catalogoCursos}">Catálogo de cursos</a>
+        <a href="${URLS.gymEntrenamiento}">GYM Entrenamiento</a>
+      </nav>
+    </main>
   `;
 })();
